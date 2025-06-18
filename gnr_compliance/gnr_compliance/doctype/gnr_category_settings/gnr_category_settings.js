04a6d765d48214db3091d97d74f5975823f88198
%phamed <mokachtit@gmail.com>
2025-06-18 08:58:01 +0000

    added doctype

---
+++ gnr_compliance/gnr_compliance/doctype/gnr_category_settings/gnr_category_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Mohamed Kachtit and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("GNR Category Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
