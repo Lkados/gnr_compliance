4a699a1c16a12ea70fddfe36bb00742cb1e5300f
Lkados <mokachtit@gmail.com>
2025-06-29 21:25:29 +0200

    fix stock entries

--- gnr_compliance/gnr_compliance/doctype/mouvement_gnr/mouvement_gnr.js
+++ gnr_compliance/gnr_compliance/doctype/mouvement_gnr/mouvement_gnr.js
@@ -1,8 +1,40 @@
-// Copyright (c) 2025, Mohamed Kachtit and contributors
-// For license information, please see license.txt
+frappe.ui.form.on("Mouvement GNR", {
+	code_produit: function (frm) {
+		if (frm.doc.code_produit) {
+			frappe.call({
+				method: "frappe.client.get_value",
+				args: {
+					doctype: "Item",
+					fieldname: "gnr_tax_rate",
+					filters: { name: frm.doc.code_produit },
+				},
+				callback: function (r) {
+					if (r.message && r.message.gnr_tax_rate) {
+						frm.set_value("taux_gnr", r.message.gnr_tax_rate);
+						calculer_montant_taxe(frm);
+					}
+				},
+			});
+		}
+	},
 
-// frappe.ui.form.on("Mouvement GNR", {
-// 	refresh(frm) {
+	quantite: function (frm) {
+		calculer_montant_taxe(frm);
+	},
 
-// 	},
-// });
+	taux_gnr: function (frm) {
+		calculer_montant_taxe(frm);
+	},
+});
+
+function calculer_montant_taxe(frm) {
+	if (frm.doc.quantite && frm.doc.taux_gnr) {
+		let montant = frm.doc.quantite * frm.doc.taux_gnr;
+		frm.set_value("montant_taxe_gnr", montant);
+
+		frappe.show_alert({
+			message: `${frm.doc.quantite}L × ${frm.doc.taux_gnr}€/L = ${montant.toFixed(2)}€`,
+			indicator: "green",
+		});
+	}
+}
